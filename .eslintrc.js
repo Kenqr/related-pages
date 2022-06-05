6a316309de979bd49916f073013b9ba7cfb0c34a
Ken <ken75113@gmail.com>
2022-06-05 16:00:47 +0800

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+// https://eslint.org/docs/user-guide/configuring
+
+/* global module */
+module.exports = {
+  extends: 'eslint:recommended',
+  env: {
+    browser: true,
+    es2020: true,
+    webextensions: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-var': 'error',
+    'no-console': ['warn', {allow: ['warn', 'error']}],
+  },
+};
